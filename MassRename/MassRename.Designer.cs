15a24ba9854a79888f1ec9edd270171512015edd
HamidReza <hamidreza2623@gmail.com>
2023-08-13 18:17:24 +0330

    Add project files.

---
+++ MassRename/MassRename.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace MassRename
+{
+    partial class MassRename
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BrowseBtn
+            // 
+            this.BrowseBtn.Location = new System.Drawing.Point(13, 13);
+            this.BrowseBtn.Name = "BrowseBtn";
+            this.BrowseBtn.Size = new System.Drawing.Size(179, 23);
+            this.BrowseBtn.TabIndex = 0;
+            this.BrowseBtn.Text = "Browse for a directory...";
+            this.BrowseBtn.UseVisualStyleBackColor = true;
+            this.BrowseBtn.Click += new System.EventHandler(this.BrowseBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1256, 394);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // MassRename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BrowseBtn);
+            this.Name = "MassRename";
+            this.Text = "MassRename";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ MassRename/MassRename.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MassRename
+{
+    public partial class MassRename : Form
+    {
+        public MassRename()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BrowseBtn_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    foreach(string filename in files)
+                    
+                        listBox1.Items.Add(filename);
+                    }
+
+                }
+            }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.Items[listBox1.SelectedIndex].ToString());
+        }
+    }
+
+}
+
